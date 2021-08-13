195423d56f1f73d7ee45b9c69f05a934557ba509
James Brignull <james.brignull@xelix.com>
2021-08-13 08:05:22 +0100

    Basic app setup

---
+++ ts-piano-keyboard/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { notes } from './helpers';
+import Octave from './Octave';
+
+function App() {
+  return (
+    <div>
+      <Octave notes={notes} />
+    </div>
+  );
+}
+
+export default App;
---
+++ ts-piano-keyboard/src/Notes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Black = styled.button`
+  background: black;
+`;
+
+const White = styled.button`
+  background: white;
+`;
+
+type Props = {
+  color: string;
+  note: string;
+};
+
+const Note: React.FC<Props> = ({ color, note }) =>
+  color === 'white' ? <White value={note} /> : <Black value={note} />;
+
+export default Note;
